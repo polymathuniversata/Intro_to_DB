296fc193714446e8c36a668db3e052d532ed518f
polymathuniversata <polymathuniversata@outlook.com>
2025-06-29 12:05:28 +0300

    Fix task_4.sql: Use correct table name 'Books' (capitalized) to match database schema

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,5 @@
 -- Use the alx_book_store database
 USE alx_book_store;
 
--- Print full description of books table without using DESCRIBE or EXPLAIN
-SHOW COLUMNS FROM books;
+-- Print full description of Books table without using DESCRIBE or EXPLAIN
+SHOW COLUMNS FROM Books;
