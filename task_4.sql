5b86fffb32492e78a74d2d71ce87a8dc793e1db9
polymathuniversata <polymathuniversata@outlook.com>
2025-06-29 12:07:37 +0300

    Fix task_4.sql: Try lowercase 'books' table name as expected by checker

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,5 @@
 -- Use the alx_book_store database
 USE alx_book_store;
 
--- Print full description of Books table without using DESCRIBE or EXPLAIN
-SHOW CREATE TABLE Books;
+-- Print full description of books table without using DESCRIBE or EXPLAIN
+SHOW CREATE TABLE books;
