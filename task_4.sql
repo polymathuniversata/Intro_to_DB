2b353dec0ad88d75144f73c8dd25279f0c943f22
polymathuniversata <polymathuniversata@outlook.com>
2025-06-29 12:08:08 +0300

    Simplify task_4.sql: Remove USE statement, keep only SHOW COLUMNS command

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,2 @@
--- Use the alx_book_store database
-USE alx_book_store;
-
 -- Print full description of books table without using DESCRIBE or EXPLAIN
-SHOW CREATE TABLE books;
+SHOW COLUMNS FROM books;
